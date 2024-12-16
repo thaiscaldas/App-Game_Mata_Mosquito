1443fba9d7c3543c40214faa91a4d95020c2cf75
Thais Caldas <caldasthais097@gmail.com>
2024-12-16 17:02:40 -0300

    Dimensões e criação de posição

---
+++ jogo.js
@@ -0,0 +1,26 @@
+let altura = 0
+let largura = 0
+
+function ajustaTamanhoPalcoJogo() {
+    altura = window.innerHeight
+    largura = window.innerWidth
+
+}
+
+ajustaTamanhoPalcoJogo()
+
+
+let posicaoX = Math.floor(Math.random() * largura) - 90
+let posicaoY = Math.floor(Math.random() * altura) - 90
+
+posicaoX = posicaoX < 0 ? 0 : posicaoX
+posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+let mosquito = document.createElement('img')
+mosquito.src = 'imagens/mosquito.png'
+mosquito.className = 'mosquito1'
+mosquito.style.left = posicaoX + 'px'
+mosquito.style.top = posicaoY + 'px'
+mosquito.style.position = 'absolute'
+
+document.body.appendChild(mosquito)
