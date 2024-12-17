a2f2aebade8655fae4e68837d6e29652d69fa43f
Thais Caldas <caldasthais097@gmail.com>
2024-12-17 15:43:55 -0300

    Criação e remoção do elemento mosquito a cada ciclo de tempo

--- jogo.js
+++ jogo.js
@@ -9,23 +9,30 @@ function ajustaTamanhoPalcoJogo() {
 
 ajustaTamanhoPalcoJogo()
 
+function posicaoRandomica() {
+    
+    if(document.getElementById('mosquito')) {  // remoção do mosquito anterior
+        document.getElementById('mosquito').remove()
+    }
 
-let posicaoX = Math.floor(Math.random() * largura) - 90
-let posicaoY = Math.floor(Math.random() * altura) - 90
+    let posicaoX = Math.floor(Math.random() * largura) - 90
+    let posicaoY = Math.floor(Math.random() * altura) - 90
 
-posicaoX = posicaoX < 0 ? 0 : posicaoX
-posicaoY = posicaoY < 0 ? 0 : posicaoY
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
 
+    //Elemento HTML dentro do script JS
+    let mosquito = document.createElement('img')
+    mosquito.src = 'imagens/mosquito.png'
+    mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+    mosquito.id = 'mosquito'
 
-//Elemento HTML dentro do script JS
-let mosquito = document.createElement('img')
-mosquito.src = 'imagens/mosquito.png'
-mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
-mosquito.style.left = posicaoX + 'px'
-mosquito.style.top = posicaoY + 'px'
-mosquito.style.position = 'absolute'
+    document.body.appendChild(mosquito)
 
-document.body.appendChild(mosquito)
+}
 
 function tamanhoAleatorio() {
     let classe = Math.floor(Math.random() * 3)
