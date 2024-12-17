2d7be410bafd4dacc33df1f129e877dd894bc3ab
Thais Caldas <caldasthais097@gmail.com>
2024-12-17 14:27:37 -0300

    Definição de tamanhos com efeitos randômicos

--- jogo.js
+++ jogo.js
@@ -16,11 +16,32 @@ let posicaoY = Math.floor(Math.random() * altura) - 90
 posicaoX = posicaoX < 0 ? 0 : posicaoX
 posicaoY = posicaoY < 0 ? 0 : posicaoY
 
+
+//Elemento HTML dentro do script JS
 let mosquito = document.createElement('img')
 mosquito.src = 'imagens/mosquito.png'
-mosquito.className = 'mosquito1'
+mosquito.className = tamanhoAleatorio()
 mosquito.style.left = posicaoX + 'px'
 mosquito.style.top = posicaoY + 'px'
 mosquito.style.position = 'absolute'
 
 document.body.appendChild(mosquito)
+
+function tamanhoAleatorio() {
+    let classe = Math.floor(Math.random() * 3)
+    
+    switch(classe) {
+        case 0:
+            return 'mosquito1'
+
+        case 1:
+            return 'mosquito2'
+
+        case 2:
+            return 'mosquito3'
+    }
+
+    console.log(classe)
+}
+
+tamanhoAleatorio()
